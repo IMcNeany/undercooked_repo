05c5760aaffc1d7d59f0ee8e65fd886ec811d6b3
D'Arcy Sullivan <d_sullivan@hotmail.co.uk>
2018-10-02 20:18:35 +0100

    Menu transition fix for Patrick
    menu-test can be removed when done with

--- undercooked/Assets/Scripts/MenuManager.cs
+++ undercooked/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
         public int player = 0;
 public void Player1()
     {
-        Application.LoadLevel("Game");
+        SceneManager.LoadScene("Level1");
         player = 1;
     }
   public void Player2()
     {
-        Application.LoadLevel("Game");
+        SceneManager.LoadScene("Level2");
         player = 2;
     }
 }
