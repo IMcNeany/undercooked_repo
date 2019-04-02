c43cfaf7604b0b2ed54f15a23abf1d34c3b64389
Unknown <nathan@thebarnetts.co.uk>
2019-04-02 13:40:16 +0100

    menus work  build

---
+++ undercooked/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class SwitchScene : MonoBehaviour {
+    public GameObject offCanvas;
+    public GameObject onCanvas;
+    public GameObject FirstObject;
+
+
+    public void Switch()
+
+    {
+        offCanvas.SetActive(true);
+        onCanvas.SetActive(false);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(FirstObject, null);
+    }
+}
